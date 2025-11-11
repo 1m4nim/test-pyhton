a5e70c2895fe6309368646c6dd4adbe8fc123134
1m4nim <fancustard9@gmail.com>
2025-11-11 14:39:50 +0900

    421

---
+++ solve.py
@@ -0,0 +1,47 @@
+import sys
+
+try:
+    X,Y=map(int, sys.stdin.read().split())
+except:
+    X,Y=1,1
+
+def solve():
+    def f(x:int)->int:
+        s_x=str(x)
+        rev_s_x=s_x[::-1]
+        return int(rev_s_x)
+    
+    A=[0]*10
+
+    A[0]=X
+    A[1]=Y
+
+    for i in range(2,10):
+        next_term=f(A[i-1]+A[i-2])
+        A[i]=next_term
+
+
+    print(A[9])
+
+solve()
+
+if __name__=="__main__":
+    try:
+        input_data=sys.stdin.read().split()
+        if len(input_data)>=2:
+            X=int(input_data[0])
+            Y=int(input_data[1])
+
+            A=[0]*10
+            A[0]=x
+            A[1]=Y
+
+            for i in range(2,10):
+                A[i]=f(A[i-1]+A[i-2])
+
+            print(A[9])
+
+    except EOFError:
+        pass
+    except ValueError:
+        pass
