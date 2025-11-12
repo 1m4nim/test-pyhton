1e23870d5da40bbad8da3e78e7731c3c8dcfc915
1m4nim <fancustard9@gmail.com>
2025-11-12 14:47:46 +0900

    vendingmachine

--- solve.py
+++ solve.py
@@ -47,12 +47,26 @@
 #         pass
 
 
-for i in range(1,101):
-    if(i%3==0)and(i%5==0):
-        print("FizzBuzz", end=" ")
-    elif(i%3==0):
-        print("Fizz", end=" ")
-    elif(i%5==0):
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+# for i in range(1,101):
+#     if(i%3==0)and(i%5==0):
+#         print("FizzBuzz", end=" ")
+#     elif(i%3==0):
+#         print("Fizz", end=" ")
+#     elif(i%5==0):
+#         print("Buzz", end=" ")
+#     else:
+#         print(i, end=" ")
+
+
+input_price=input("insert: ")
+product_price=input("product: ")
+change=int(input_price)-int(product_price)
+
+coin=[5000,1000,500,100,50,10,5,1]
+
+for i in coin:
+    r=change//i
+    change=change%i
+    print(str(i)+":"+str(r))
+
+    
