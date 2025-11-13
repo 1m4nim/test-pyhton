8b31ed7fbe7cdf64857ddcc1852a96de53885b87
1m4nim <fancustard9@gmail.com>
2025-11-13 11:50:01 +0900

    忘れてた

--- solve.py
+++ solve.py
@@ -1,32 +1,32 @@
-def fibonacci(n):
-    if (n == 1) or (n == 2):
-        return 1
-    return fibonacci(n - 2) + fibonacci(n - 1)
+# def fibonacci(n):
+#     if (n == 1) or (n == 2):
+#         return 1
+#     return fibonacci(n - 2) + fibonacci(n - 1)
 
 
-print(fibonacci(6))
+# print(fibonacci(6))
 
-# メモ化
-memo = {1: 1, 2: 1}
-
-
-def fibonacci(n):
-    if n in memo:
-        return memo[n]
-
-    memo[n] = fibonacci(n - 2) + fibonacci(n - 1)
-    return memo[n]
+# # メモ化
+# memo = {1: 1, 2: 1}
 
 
-print(fibonacci(6))
+# def fibonacci(n):
+#     if n in memo:
+#         return memo[n]
+
+#     memo[n] = fibonacci(n - 2) + fibonacci(n - 1)
+#     return memo[n]
 
 
-# ループ
-def fibonacci(n):
-    fib = [1, 1]
-    for i in range(2, n):
-        fib.append(fib[i - 2] + fib[i - 1])
-    return fib[n - 1]
+# print(fibonacci(6))
 
 
-print(fibonacci(6))
+# # ループ
+# def fibonacci(n):
+#     fib = [1, 1]
+#     for i in range(2, n):
+#         fib.append(fib[i - 2] + fib[i - 1])
+#     return fib[n - 1]
+
+
+# print(fibonacci(6))
