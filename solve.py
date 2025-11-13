d952ad6012749dacad77ea1b109088d32268c00d
1m4nim <fancustard9@gmail.com>
2025-11-13 09:41:59 +0900

    fibonacci

--- solve.py
+++ solve.py
@@ -1,96 +1,32 @@
-# import sys
-
-# try:
-#     X,Y=map(int, sys.stdin.read().split())
-# except:
-#     X,Y=1,1
-
-# def solve():
-#     def f(x:int)->int:
-#         s_x=str(x)
-#         rev_s_x=s_x[::-1]
-#         return int(rev_s_x)
-    
-#     A=[0]*10
-
-#     A[0]=X
-#     A[1]=Y
-
-#     for i in range(2,10):
-#         next_term=f(A[i-1]+A[i-2])
-#         A[i]=next_term
+def fibonacci(n):
+    if (n == 1) or (n == 2):
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1)
 
 
-#     print(A[9])
+print(fibonacci(6))
 
-# solve()
-
-# if __name__=="__main__":
-#     try:
-#         input_data=sys.stdin.read().split()
-#         if len(input_data)>=2:
-#             X=int(input_data[0])
-#             Y=int(input_data[1])
-
-#             A=[0]*10
-#             A[0]=x
-#             A[1]=Y
-
-#             for i in range(2,10):
-#                 A[i]=f(A[i-1]+A[i-2])
-
-#             print(A[9])
-
-#     except EOFError:
-#         pass
-#     except ValueError:
-#         pass
+# メモ化
+memo = {1: 1, 2: 1}
 
 
-# for i in range(1,101):
-#     if(i%3==0)and(i%5==0):
-#         print("FizzBuzz", end=" ")
-#     elif(i%3==0):
-#         print("Fizz", end=" ")
-#     elif(i%5==0):
-#         print("Buzz", end=" ")
-#     else:
-#         print(i, end=" ")
+def fibonacci(n):
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fibonacci(n - 2) + fibonacci(n - 1)
+    return memo[n]
 
 
-# input_price=input("insert: ")
-# product_price=input("product: ")
-# change=int(input_price)-int(product_price)
+print(fibonacci(6))
 
-# coin=[5000,1000,500,100,50,10,5,1]
 
-# for i in coin:
-#     r=change//i
-#     change=change%i
-#     print(str(i)+":"+str(r))
+# ループ
+def fibonacci(n):
+    fib = [1, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 2] + fib[i - 1])
+    return fib[n - 1]
 
-    
-import sys
 
-input_price=input("insert: ")
-if not input_price.isdecimal():
-    print("整数を入力してください")
-    sys.exit()
-
-product_price=input("product: ")
-if not product_price.isdecimal():
-    print("整数を入力してください")
-    sys.exit()
-
-change=int(input_price)-int(product_price)
-
-if change<0:
-    print("お金足りないよ")
-    sys.exit()
-
-coin = [5000,1000,500,100,50,10,5,1]
-
-for i in coin:
-    r=change//i
-    change=change%i
-    print(str)(i)+":"+str(r)
+print(fibonacci(6))
